0f67e36ecd6592d26328ca47857a1711c29d6a2d
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 01:29:58 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,16 +9,13 @@ app = FastAPI(title="Notes API with Version History")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    "http://localhost:5173",
+    "https://notes-frontend.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=[
-    "http://localhost:5173",
-    "https://notes-frontend.vercel.app",
-    ]
+    
 )
 
 
