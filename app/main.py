493f2f5c4c28f16f07aaff7f1a9272d1c14b9205
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 02:08:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,27 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth, notes, versions
+from app.db.session import engine
+from app.db.base import Base   # 👈 IMPORTANT
+
+# ✅ CREATE DATABASE TABLES
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Notes API with Version History")
-
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "https://notes-frontend.vercel.app",
-],
+        "http://localhost:5173",
+        "https://notes-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    
 )
-
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(notes.router, prefix="/notes", tags=["Notes"])
 app.include_router(versions.router, tags=["Versions"])
-
 
 @app.get("/")
 def health():
