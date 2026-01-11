0a58a49c76c1e32514c8767c3fb4b7dfc823a324
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 02:10:49 +0530

    Update base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,9 @@
 from sqlalchemy.orm import DeclarativeBase
+from app.db.base_class import Base
+
+from app.models.user import User
+from app.models.note import Note
+
 
 class Base(DeclarativeBase):
     pass
