157c5649dc3c4b265ef53683e9bfd70d6163cf75
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 02:36:12 +0530

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,10 +1,2 @@
-from app.db.base import Base
-from app.db.session import engine
 
-
-from app.models.user import User
-from app.models.note import Note
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
