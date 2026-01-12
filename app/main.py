614890834c4150dc723dbbb64a926ca7cb92b169
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 05:38:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,12 +25,8 @@ init_db()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
     allow_origin_regex=r"https://notes-frontend-.*\.vercel\.app",
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
