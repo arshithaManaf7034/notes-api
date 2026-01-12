31d13f7853e57ca72e56642f401081f5b28c4a88
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-01-12 05:30:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,8 +28,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://notes-frontend-4py8.vercel.app",
     ],
+    allow_origin_regex=r"https://notes-frontend-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
